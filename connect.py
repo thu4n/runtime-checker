2c02a3d903c94a594e242de8ba771e7622ab0380
thu4n <thuan.tongg@gmail.com>
2024-11-26 21:04:43 +0700

    workflow for socket

---
+++ connect.py
@@ -0,0 +1,23 @@
+import asyncio
+import socketio
+
+sio = socketio.AsyncClient()
+
+@sio.event
+async def connect():
+    print("Connected to the server!")
+
+@sio.event
+async def disconnect():
+    print("Disconnected from the server!")
+
+async def start_connection():
+    try:
+        await sio.connect('https://kit.digitalauto.tech', wait_timeout=10)
+        print("Connected successfully!")
+        await sio.wait()
+    except Exception as e:
+        print(f"Failed to connect: {e}")
+
+if __name__ == "__main__":
+    asyncio.run(start_connection())
